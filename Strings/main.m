5939fde85d2ba58453361678e74b089bad3b17a1
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-04 11:33:19 -0700

    Initial Commit

---
+++ Strings/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Strings
+//
+//  Created by Jamie on 2018-07-04.
+//  Copyright © 2018 Jamie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
